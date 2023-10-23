917b0b5a57e63590ee0969626dbd057af8bd08ce
Stella <renew.stella@gmail.com>
2023-10-23 16:55:58 +0900

    인기 검색어 repo 완료

--- src/main/java/com/dog/shop/repository/crawling/PopularKeywordRepository.java
+++ src/main/java/com/dog/shop/repository/crawling/PopularKeywordRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 public interface PopularKeywordRepository extends JpaRepository<PopularSearchedKeyword, Integer> {
 
-//    public Optional<PopularSearchedKeyword> findAllByOrderBykeywordCountDesc();
 
 }
