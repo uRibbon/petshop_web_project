10cf22634b23aaf31ac01a8fd192024a5fb74029
Stella <renew.stella@gmail.com>
2023-10-24 11:08:20 +0900

    상품 문의 controller 전체 조회, 개별 조회, 삭제, 수정 기능 구현

--- src/main/java/com/dog/shop/web/inquiry/InquiryController.java
+++ src/main/java/com/dog/shop/web/inquiry/InquiryController.java
@@ -1,14 +1,17 @@
 package com.dog.shop.web.inquiry;
 
+import com.dog.shop.domain.inquiry.Inquiry;
 import com.dog.shop.domain.inquiry.reqDTO.InquiryReqDTO;
 import com.dog.shop.domain.inquiry.resDTO.InquiryResDTO;
 import com.dog.shop.service.inquiry.InquiryService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Book;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inquiry")
@@ -19,10 +22,38 @@ public class InquiryController {
 
     private final ModelMapper modelMapper;
 
+
     // 등록
     @PostMapping
-    public InquiryResDTO regInquiry(@RequestBody InquiryReqDTO inquiryReqDTO) {
-        return inquiryService.regInquiry(inquiryReqDTO);
+    public ResponseEntity<InquiryResDTO> registerInquiry(@RequestBody InquiryReqDTO inquiryReqDTO) {
+        // userId 값을 2L로 고정
+        Long userId = 2L;
+        InquiryResDTO result = inquiryService.regInquiry(userId, inquiryReqDTO);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    // 전체 조회
+    @GetMapping
+    public List<InquiryResDTO> getInquiryies() {
+        return inquiryService.getInquiries();
+    }
+
+    // id로 개별 조회
+    @GetMapping("/{id}")
+    public InquiryResDTO getInquiryById(@PathVariable Long id) {
+        return inquiryService.getInquiryById(id);
+    }
+
+    // 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteInquiry(@PathVariable Long id) {
+        inquiryService.deleteInquiry(id);
+        return ResponseEntity.ok(id + " 번째 책이 삭제처리 되었습니다.");
+    }
+
+    // 수정
+    @PatchMapping("/{id}")
+    public InquiryResDTO updateInquiry(@PathVariable Long id, @RequestBody InquiryReqDTO inquiryReqDTO) {
+        return inquiryService.updateInquiry(id, inquiryReqDTO);
+    }
 }
