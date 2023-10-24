fef30ed93f3cae92d54fb0f9d12090437e8012f8
Stella <renew.stella@gmail.com>
2023-10-24 09:18:46 +0900

    상품 문의 entity 수정

--- src/main/java/com/dog/shop/domain/Inquiry.java
+++ src/main/java/com/dog/shop/domain/Inquiry.java
@@ -1,5 +1,7 @@
 package com.dog.shop.domain;
 
+import com.dog.shop.myenum.InquiryStatus;
+import com.dog.shop.myenum.SalesStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,7 +22,11 @@ public class Inquiry {
 
     private String title; // 문의 제목
     private String content; // 문의 내용
-    private String status; // 문의 처리 상태 Enum으로 변경 예정
+//    private String status; // 문의 처리 상태 Enum으로 변경 예정
+
+    @Enumerated(EnumType.STRING)
+    private InquiryStatus inquiryStatus; // 문의 처리 상태
+
     private String response; // 관리자의 답변 내용
     private LocalDateTime responseDate; // 답변이 작성된 날짜와 시간
 
