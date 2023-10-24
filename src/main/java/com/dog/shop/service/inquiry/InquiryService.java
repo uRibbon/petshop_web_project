247c75cb878970af94db70a47e4c93882bc5b10b
Stella <renew.stella@gmail.com>
2023-10-24 11:08:57 +0900

    상품 문의 service 전체 조회, 개별 조회, 삭제, 수정 기능 구현

--- src/main/java/com/dog/shop/service/inquiry/InquiryService.java
+++ src/main/java/com/dog/shop/service/inquiry/InquiryService.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.awt.print.Book;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @Transactional
@@ -23,11 +26,48 @@ public class InquiryService {
     private final ModelMapper modelMapper;
 
     // 등록
-    public InquiryResDTO regInquiry(InquiryReqDTO inquiryReqDTO) {
+    public InquiryResDTO regInquiry(Long userId, InquiryReqDTO inquiryReqDTO) {
         Inquiry inquiry = modelMapper.map(inquiryReqDTO, Inquiry.class);
         Inquiry savedInquiry = inquiryRepository.save(inquiry);
         return modelMapper.map(savedInquiry, InquiryResDTO.class);
     }
 
+    // 전체 조회
+    @Transactional(readOnly = true)
+    public List<InquiryResDTO> getInquiries() {
+        List<Inquiry> inquiryList = inquiryRepository.findAll();
+
+        List<InquiryResDTO> inquiryResDTOList = inquiryList.stream()
+                .map(inquiry -> modelMapper.map(inquiry, InquiryResDTO.class))
+                .collect(toList());
+
+        return inquiryResDTOList;
+    }
+
+    // Id로 조회
+    @Transactional(readOnly = true)
+    public InquiryResDTO getInquiryById(Long id) {
+        Inquiry inquiry = inquiryRepository.findById(id).orElseThrow();
+        InquiryResDTO inquiryResDTO = modelMapper.map(inquiry, InquiryResDTO.class);
+        return inquiryResDTO;
+    }
+
+    // 삭제
+    public void deleteInquiry(Long id) {
+        Inquiry inquiry = inquiryRepository.findById(id).orElseThrow();
+        inquiryRepository.delete(inquiry);
+    }
+
+    // 수정
+    public InquiryResDTO updateInquiry(Long id, InquiryReqDTO inquiryReqDTO) {
+        Inquiry existInquiry = inquiryRepository.findById(id).orElseThrow();
+        if(inquiryReqDTO.getTitle() != null) {
+            existInquiry.setTitle(inquiryReqDTO.getTitle());
+        }
+        if(inquiryReqDTO.getContent() != null) {
+            existInquiry.setContent(inquiryReqDTO.getContent());
+        }
+        return modelMapper.map(existInquiry, InquiryResDTO.class);
+    }
 
 }
