9a5a8739641ce2565da66662de862ed15630a59b
Jueun <heojueun0316@gmail.com>
2023-10-24 10:40:03 +0900

    완료
    완료

--- src/main/java/com/dog/shop/config/NewSecuityConfig.java
+++ src/main/java/com/dog/shop/config/NewSecuityConfig.java
@@ -35,7 +35,7 @@ public class NewSecuityConfig {
                 .exceptionHandling(
                         exceptionHandling -> exceptionHandling.authenticationEntryPoint(customAuthenticationEntryPoint))
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/api/register").permitAll()
+                        .requestMatchers("/api/register", "/auth/**").permitAll()
                         .requestMatchers("/user/**").authenticated()
                 )
                 .logout(logout -> logout
--- src/main/java/com/dog/shop/domain/User.java
+++ src/main/java/com/dog/shop/domain/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table
