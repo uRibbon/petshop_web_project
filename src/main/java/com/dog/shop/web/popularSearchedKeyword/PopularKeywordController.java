347f509493bb15112735a57f4182a16049b5802e
Stella <renew.stella@gmail.com>
2023-10-25 14:43:32 +0900

    인기검색어 앞단 controller 생성

---
+++ src/main/java/com/dog/shop/web/popularSearchedKeyword/PopularKeywordController.java
@@ -0,0 +1,27 @@
+package com.dog.shop.web.popularSearchedKeyword;
+
+import com.dog.shop.domain.popularSearchedKeyword.resDTO.PopularSearchedKeywordResDTO;
+import com.dog.shop.service.popularKeyword.PopularKeywordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/keywords")
+@RequiredArgsConstructor
+public class PopularKeywordController {
+
+    private final PopularKeywordService popularKeywordService;
+
+    // 인기 검색어 보여주기
+    @GetMapping("/list")
+    public ModelAndView keywordList() {
+        List<PopularSearchedKeywordResDTO> popularSearchedKeywordResDTOList = popularKeywordService.getResult();
+        return new ModelAndView("/PopularSearchedKeyword/popular-searched-keyword", "keywords", popularSearchedKeywordResDTOList);
+
+    }
+}
