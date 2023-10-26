39cb15b0f694dfc516f75d85b8dda370606904c0
HYUNJUN123456 <juni303@chauniv.ac.kr>
2023-10-26 14:24:08 +0900

    필요한 빈 추가

--- src/main/java/com/dog/shop/ShopProjectApplication.java
+++ src/main/java/com/dog/shop/ShopProjectApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -22,6 +25,16 @@ public class ShopProjectApplication {
 		return modelMapper;
 	}
 
+
 	@Bean
-	public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public WebClient.Builder webClientBuilder() {
+		return WebClient.builder();
+	}
+
+
 }
