f4ffd4e437aeab5b756bcd9ae0c673c602aaf58c
HYUNJUN123456 <juni303@chauniv.ac.kr>
2023-10-27 13:09:29 +0900

    장바구니 로직 부분 변경

--- src/main/java/com/dog/shop/repository/CartItemRepository.java
+++ src/main/java/com/dog/shop/repository/CartItemRepository.java
@@ -1,10 +1,14 @@
 package com.dog.shop.repository;
 
-import com.dog.shop.domain.CartItem;
+import com.dog.shop.domain.cart.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+import java.util.List;
 
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+
+    List<CartItem> findByCartId(Long id);
 }
