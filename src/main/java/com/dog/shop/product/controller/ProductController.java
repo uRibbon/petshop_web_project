7b44580029bce3e0fa34b07cb9dd285fd102a148
heogoodman <tjdwo1030@naver.com>
2023-10-30 15:33:08 +0900

    서버 이미지업로드성공(포스트맨)

--- src/main/java/com/dog/shop/product/controller/ProductController.java
+++ src/main/java/com/dog/shop/product/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.dog.shop.product.dto.ProductReqDTO;
 import com.dog.shop.product.dto.ProductReqForm;
 import com.dog.shop.product.dto.ProductResDTO;
 import com.dog.shop.product.service.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -11,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -36,17 +39,21 @@ public class ProductController {
     }
 
     @PostMapping("/create")
-    public String createProduct(@Valid ProductReqDTO product,@RequestParam("mainImage") MultipartFile file , BindingResult result) {
-        if (result.hasErrors()) {
-            return "create-product";
+    public String createProduct(
+            @RequestParam("product") String productJson,
+            @RequestParam(name = "mainImage", required = false) MultipartFile file) throws JsonProcessingException {
+
+        // ObjectMapper를 사용하여 JSON 문자열을 DTO 객체로 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        ProductReqDTO productReqDTO = objectMapper.readValue(productJson, ProductReqDTO.class);
+        System.out.println(productReqDTO);
+
+        if (file != null && !file.isEmpty()) {
+            String imageUrl = productService.saveImage(file);
+            productReqDTO.setMainImageUrl(imageUrl);
         }
-        // 이미지를 저장하고 URL을 반환하는 메서드를 productService에서 구현합니다.
-        String imageUrl = productService.saveImage(file);
 
-        // productReqDTO에 이미지 커스텀마이징한 imageUrl 정보 삽입
-        product.setMainImageUrl(imageUrl);
-
-        productService.saveProduct(product);
+        productService.saveProduct(productReqDTO);
         return "redirect:/products/list";
     }
 
