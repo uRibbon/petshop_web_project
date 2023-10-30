4d0b1ab832fddf095223a923eae861cd1de9b106
Jueun <heojueun0316@gmail.com>
2023-10-30 09:15:44 +0900

    orderRepositoy 삭제
    orderRepository 삭제, 리뷰 수정

--- src/main/java/com/dog/shop/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dog.shop.repository;
-
-import com.dog.shop.domain.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderItem, Long> {
-
-}
