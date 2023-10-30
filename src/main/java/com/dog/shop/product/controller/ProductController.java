8802153c712b6338221b81da6875a831beaeab25
heogoodman <tjdwo1030@naver.com>
2023-10-30 17:51:05 +0900

    fail

--- src/main/java/com/dog/shop/product/controller/ProductController.java
+++ src/main/java/com/dog/shop/product/controller/ProductController.java
@@ -13,7 +13,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
