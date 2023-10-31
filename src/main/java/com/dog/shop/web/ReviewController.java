6bba9d64f0015aa851bd88f99e2c18a7835342d2
Jueun <heojueun0316@gmail.com>
2023-10-31 13:04:25 +0900

    QnA Res 에 id 추가함

--- src/main/java/com/dog/shop/web/ReviewController.java
+++ src/main/java/com/dog/shop/web/ReviewController.java
@@ -5,7 +5,6 @@ import com.dog.shop.dto.reviewDto.ReviewReqDto;
 import com.dog.shop.dto.reviewDto.ReviewResDto;
 import com.dog.shop.help.JwtHelper;
 import com.dog.shop.service.ReviewService;
-import com.dog.shop.utils.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -93,6 +92,7 @@ public class ReviewController {
     }
 
     // 리뷰 가져오기
+    @ResponseBody
     @GetMapping("/reviewList")
     public List<ReviewResDto> getReview() {
         return reviewService.showAllReview();
