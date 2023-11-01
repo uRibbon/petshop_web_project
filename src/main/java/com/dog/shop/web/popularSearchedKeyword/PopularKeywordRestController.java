ae9907e096664089e8724479e8ae9da74a000189
Stella <renew.stella@gmail.com>
2023-11-01 15:16:00 +0900

    인기 검색어 앞단 수정

--- src/main/java/com/dog/shop/web/popularSearchedKeyword/PopularKeywordRestController.java
+++ src/main/java/com/dog/shop/web/popularSearchedKeyword/PopularKeywordRestController.java
@@ -1,9 +1,9 @@
 package com.dog.shop.web.popularSearchedKeyword;
 
+
 import com.dog.shop.domain.popularSearchedKeyword.resDTO.PopularSearchedKeywordResDTO;
 import com.dog.shop.service.popularKeyword.PopularKeywordService;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +17,10 @@ public class PopularKeywordRestController {
 
     private final PopularKeywordService popularKeywordService;
 
-    private final ModelMapper modelMapper;
-
-
-    // 전체 조회
+   // 전체 조회
     @GetMapping
     public List<PopularSearchedKeywordResDTO> getResults() {
         return popularKeywordService.getResult();
     }
+
 }
