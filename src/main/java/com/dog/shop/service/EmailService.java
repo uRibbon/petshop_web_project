f184fbe8a543eee55cfa0ae93c911b590c5e2da2
HYUNJUN123456 <juni303@chauniv.ac.kr>
2023-11-02 11:27:13 +0900

    패스워드 일단 푸시

--- src/main/java/com/dog/shop/service/EmailService.java
+++ src/main/java/com/dog/shop/service/EmailService.java
@@ -36,6 +36,24 @@ public class EmailService {
         mailSender.send(message);
     }
 
+    @Async
+    public void sendMailPasswordChange(String recipient, String verificationCode) throws UnsupportedEncodingException, MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        message.addRecipients(Message.RecipientType.TO, recipient); // 받는 사람
+        message.setSubject("[FIND BY DOG 비밀번호 변경]");
+        String text = "안녕하세요, FIND BY DOG 회원님!\n\n"
+                + "귀하의 FIND BY DOG 비밀번호 변경을 위한 요청이 접수되었습니다.\n"
+                + "아래의 비밀번호를 사용하여 로그인을 완료해 주세요.\n\n"
+                + "비밀번호: " + verificationCode + "\n\n"
+                + "이 메일에 대해 알지 못하시거나 요청하지 않으셨다면, 귀하의 계정이 위험할 수 있으므로 즉시 저희에게 연락해 주십시오.\n\n"
+                + "감사합니다.\n"
+                + "FIND BY DOG 팀 드림.";
+        message.setText(text, "utf-8");
+        message.setFrom(new InternetAddress(ADMIN_ADDRESS, "FIND BY DOG")); // Optional Sender Name
+        mailSender.send(message);
+    }
+
+
     /*@Async
     public void sendMail(String recipient, String password) throws UnsupportedEncodingException, MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
---
+++ src/main/java/com/dog/shop/web/email/EmailRequest.java
@@ -0,0 +1,12 @@
+package com.dog.shop.web.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailRequest {
+    private String email;
+}
